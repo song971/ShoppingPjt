7ba9c67f26e62bc8e818eaec953d8c9bb9e2eafd
choihoon0808 <choihoon0808@naver.com>
2020-06-05 14:28:15 +0900

    컨트롤러 끝

--- ComputerShop/src/main/java/com/hardCarry/shopping/Controller/CategoryController.java
+++ ComputerShop/src/main/java/com/hardCarry/shopping/Controller/CategoryController.java
@@ -7,43 +7,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CategoryController { // 카테고리 컨트롤러
 	
-	@RequestMapping("service") // 고객센터 페이지
-	public String service(Model model) {
-		return "service";
-	}
-	
-	@RequestMapping("mywrite") // 고객센터 상세 페이지
-	public String servicewrite(Model model) {
-		return "servicewrite";
-	}
-	
-	@RequestMapping("freq") // 자주하는 질문 페이지
+	@RequestMapping("freq.do") // 자주하는 질문 페이지
 	public String freq(Model model) {
 		return "freq";
 	}
 	
-	@RequestMapping("freqwrite") // 자주하는 질문 상세 페이지
+	@RequestMapping("freqwrite.do") // 자주하는 질문 상세 페이지
 	public String freqwrite(Model model) {
 		return "freqwrite";
 	}
 	
-	@RequestMapping("Notice") // 공지사항 페이지
+	@RequestMapping("Notice.do") // 공지사항 페이지
 	public String Notice(Model model) {
 		return "Notice";
 	}
 	
-	@RequestMapping("Noticewrite") // 공지사항 상세 페이지
+	@RequestMapping("Noticewrite.do") // 공지사항 상세 페이지
 	public String Noticewrite(Model model) {
 		return "Noticewrite";
 	}
 	
-	@RequestMapping("qa") // Q & A 페이지
+	@RequestMapping("qa.do") // Q & A 페이지
 	public String qa(Model model) {
 		return "qa";
 	}
 	
-	@RequestMapping("qawrite") // Q & A 상세 페이지
-	public String qawrite(Model model) {
-		return "qawrite";
+	@RequestMapping("qaview.do") // Q & A 상세 페이지
+	public String qaview(Model model) {
+		return "qaview";
 	}
+	
 }
--- ComputerShop/src/main/java/com/hardCarry/shopping/Controller/UserController.java
+++ ComputerShop/src/main/java/com/hardCarry/shopping/Controller/UserController.java
@@ -5,59 +5,59 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class userController { // 사용자 컨트롤러
+public class UserController { // 사용자 컨트롤러
 	
-	@RequestMapping("login") // 로그인 페이지
+	@RequestMapping("login.do") // 로그인 페이지
 	public String login(Model model) {
 		return "login";
 	}
 	
-	@RequestMapping("membership") // 회원가입 페이지
+	@RequestMapping("membership.do") // 회원가입 페이지
 	public String membership(Model model) {
 		return "membership";
 	}
 	
-	@RequestMapping("findid") // 아이디 찾기 페이지
+	@RequestMapping("find.do") // 아이디, 비밀번호 찾기 페이지
 	public String findid(Model model) {
 		return "findid";
 	}
 	
-	@RequestMapping("findpw") // 비밀번호 찾기 페이지
-	public String findpw(Model model) {
-		return "findpw";
+	@RequestMapping("pwchange.do") // 비밀번호 변경 페이지
+	public String pwchange(Model model) {
+		return "pwchange";
 	}
 	
-	@RequestMapping("mypage") // My Page 
+	@RequestMapping("mypage.do") // My Page 
 	public String mypage(Model model) {
 		return "mypage";
 	}
 	
-	@RequestMapping("infochange") // 회원정보 수정 페이지
+	@RequestMapping("infochange.do") // 회원정보 수정 페이지
 	public String infochange(Model model) {
 		return "infochange";
 	}
 	
-	@RequestMapping("shoppingbasket") // 장바구니 페이지
+	@RequestMapping("shoppingbasket.do") // 장바구니 페이지
 	public String shoppingbasket(Model model) {
 		return "shoppingbasket";
 	}
 	
-	@RequestMapping("Ordering") // 주문 페이지
+	@RequestMapping("Ordering.do") // 주문 페이지
 	public String Ordering(Model model) {
 		return "Ordering";
 	}
 	
-	@RequestMapping("Ordercheck") // 주문 조회 페이지
+	@RequestMapping("Ordercheck.do") // 주문 조회 페이지
 	public String Ordercheck(Model model) {
 		return "Ordercheck";
 	}
 	
-	@RequestMapping("myview") // 내가 쓴 글 페이지
+	@RequestMapping("myview.do") // 내가 쓴 글 페이지
 	public String myview(Model model) {
 		return "myview";
 	}
 	
-	@RequestMapping("mywrite") // 내가 쓴 글 상세 페이지
+	@RequestMapping("mywrite.do") // 내가 쓴 글 상세 페이지
 	public String mywrite(Model model) {
 		return "mywrite";
 	}
