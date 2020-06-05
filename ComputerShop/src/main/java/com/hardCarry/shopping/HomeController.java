962a8aff6b7199bf29c79f128a08c8712336dd36
choihoon0808 <choihoon0808@naver.com>
2020-06-05 15:45:12 +0900

    상품 목록 페이지

--- ComputerShop/src/main/java/com/hardCarry/shopping/HomeController.java
+++ ComputerShop/src/main/java/com/hardCarry/shopping/HomeController.java
@@ -15,46 +15,34 @@ public class HomeController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-	@RequestMapping(value = "index", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody String home(@RequestBody String id) {
-		return id;
-	}
-		
 	@RequestMapping("about.do")
 	public String about(Model model) {
 		return "about";
-		}
-	
-	
+	}
+
 	@RequestMapping("computer.do")
 	public String computer(Model model) {
 		return "computer";
-		}
-	
+	}
 
 	@RequestMapping("contact.do")
 	public String contact(Model model) {
 		return "contact";
-		}
-	
-	
+	}
+
 	@RequestMapping("index.do")
 	public String index(Model model) {
 		return "index";
-		}
-	
-	
+	}
+
 	@RequestMapping("laptop.do")
 	public String laptop(Model model) {
 		return "laptop";
-		}
-	
-	
+	}
+
 	@RequestMapping("productq.do")
 	public String productq(Model model) {
 		return "productq";
-		}
-	
-	
-	
+	}
+
 }
